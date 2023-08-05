c9302637a139685e79a6d33286b5c75d85bc657e
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-05 17:39:11 -0400

    set up eslint

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-  
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
 const App = () => {
   return (
-    <View style={{
-          flex: 1,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center',
-      }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Text>Open up your asshole!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 export default App;
