7b7537c371beb21918f9628932d1051dcb1e32e9
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-05 17:34:08 -0400

    boilerplate

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+  
+const App = () => {
+  return (
+    <View style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
+      }}>
+      <Text>Open up your asshole!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default App;
