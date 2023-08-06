821324e7c65117db5868f627d18a75297ad61aee
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-06 03:45:34 -0400

    nit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import SpinningWheel from "./components/SpinningWheel";
 
 const App = () => {
